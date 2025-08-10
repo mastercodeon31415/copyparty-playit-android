1aa0a023393767d189a6025b83cbb471e6f356d5
0% [█ █ █ █ █ █ █ █ █ █] 100% <mastercodeon@gmail.com>
2025-08-10 16:39:52 -0500

    Fixes + vips dep fix

--- playit-copyparty-installer.sh
+++ playit-copyparty-installer.sh
@@ -3,32 +3,27 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Installing python and ffmpeg into Termux\n${NC}"
+printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Installing python and ffmpeg into Termux\n\n${NC}"
 apt-get install python -y
 apt-get install ffmpeg -y
 
-printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Installing Copyparty optional deps into Termux\n${NC}"
+printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Installing Copyparty optional deps into Termux\n\n${NC}"
 
-apt-get install argon2 libjpeg-turbo libwebp libheif cmake -y
+apt-get install argon2 libjpeg-turbo libwebp libheif cmake libvips -y
 pip install --upgrade Pillow
 pip install pillow-heif
 pip install pyvips
 pip install psutil
-
-# On arm64 this package stalls in installing the backend deps
 pip install pyzmq
-
-read -p "Press enter"
-
 pip install mutagen
 pip install argon2
 pip install argon2-cffi
 pip cache purge
 
-printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Going to termux bin directory\n${NC}"
+printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Going to termux bin directory\n\n${NC}"
 cd /data/data/com.termux/files/usr/bin
 
-printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Making uninstaller script\n${NC}"
+printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Making uninstaller script\n\n${NC}"
 touch ./playit-copyparty-uninstall
 cat <<EOT >> ./playit-copyparty-uninstall
 # Change directory
@@ -39,25 +34,25 @@ YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 # Remove directories and files
-printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Uninstalling PlayIt Copyparty kali chroot!\n${NC}"
+printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Uninstalling PlayIt Copyparty kali chroot!\n\n${NC}"
 rm -rf ./kali-binds
 rm -rf ./kali-fs
 rm -rf ./start-kali
-printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Uninstall finished!\n${NC}"
+printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Uninstall finished!\n\n${NC}"
 rm -rf ./playit-copyparty-uninstall
 EOT
 chmod +x ./playit-copyparty-uninstall
 
-printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Downloading\n${NC}"
+printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Downloading\n\n${NC}"
 wget https://github.com/mastercodeon31415/copyparty-playit-android/raw/refs/heads/main/kali.sh
 
-printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Executing kali installerr script\n${NC}"
+printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Executing kali installerr script\n\n${NC}"
 chmod 777 ./kali.sh
 ./kali.sh
 rm -rf ./kali.sh
 mv ./start-kali.sh ./start-kali
 
-printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Navigating to Termux home directory and downloading copyparty + making shell alias\n${NC}"
+printf "${GREEN}[###PlayIt Copyparty INSTALLER###] Navigating to Termux home directory and downloading copyparty + making shell alias\n\n${NC}"
 cd /data/data/com.termux/files/home
 
 wget https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py
@@ -67,5 +62,5 @@ chmod +x ./copyparty
 
 cd 
 
-printf "${GREEN}[###PlayIt Copyparty INSTALLER###] PlayIt Copyparty Install complete! Starting kali chroot!\n${NC}"
+printf "${GREEN}[###PlayIt Copyparty INSTALLER###] PlayIt Copyparty Install complete! Starting kali chroot!\n\n${NC}"
 start-kali
